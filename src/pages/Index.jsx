b26e6e3f397993640165ac1e1a54ffb4b357919a
Mileydi <mileydiam25@gmail.com>
2021-12-21 21:59:01 -0500

    cambio imagen proyect

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,7 +5,7 @@ const Index = () => {
     <div>
       <div className='bg-blue-white h-200'>
       <span className='my-50 text-xl font-bold text-center'>Welcome</span>
-      <img src='https://library.kissclipart.com/20181202/twe/kissclipart-project-clipart-project-management-project-manager-017c85ec63681a6d.jpg' alt='Logo' className='h-800' />
+      <img src='https://es.unesco.org/youth/toptips/user/pages/images/home-feature-two_mobile.png' alt='Logo' className='h-200' />
       
       </div>
       
